351ad4aa0bff5979d29d039b1ca702c9ed86f784
Imre Vadász <imre@vdsz.com>
2016-01-28 23:01:13 +0100

    kernel: initial version of runtime power management framework.

---
+++ sys/sys/runtime_pm.h
@@ -0,0 +1,21 @@
+#ifndef _RUNTIME_PM_H_
+#define _RUNTIME_PM_H_
+
+#ifdef _KERNEL
+
+struct rpm_ops {
+	int (*runtime_suspend) (device_t dev);
+	int (*runtime_resume) (device_t dev);
+};
+
+void pm_runtime_register(device_t dev, struct rpm_ops *ops);
+void pm_runtime_unregister(device_t dev);
+void pm_runtime_mark_last_busy(device_t dev);
+void pm_runtime_put_autosuspend(device_t dev);
+void pm_runtime_set_autosuspend_delay(device_t dev, int delay);
+void pm_runtime_get_sync(device_t dev);
+void pm_runtime_get_noresume(device_t dev);
+
+#endif /* _KERNEL */
+
+#endif /* _RUNTIME_PM_H_ */
