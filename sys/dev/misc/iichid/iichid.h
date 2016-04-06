1e823933e65a422d2340adb6a8353f2f9a22f09b
Imre Vadász <imre@vdsz.com>
2016-04-06 21:26:07 +0200

    iichid: Implement I2C HID support (for the HP X2 210 detachable).

---
+++ sys/dev/misc/iichid/iichid.h
@@ -0,0 +1,21 @@
+#ifndef _IICHID_H_
+#define _IICHID_H_
+
+struct iic_hid_descriptor {
+	uint16_t wHIDDescLength;
+	uint16_t bcdVersion;
+	uint16_t wReportDescLength;
+	uint16_t wReportDescRegister;
+	uint16_t wInputRegister;
+	uint16_t wMaxInputLength;
+	uint16_t wOutputRegister;
+	uint16_t wMaxOutputLength;
+	uint16_t wCommandRegister;
+	uint16_t wDataRegister;
+	uint16_t wVendorID;
+	uint16_t wProductID;
+	uint16_t wVersionID;
+	uint32_t reserved;
+} __packed;
+
+#endif /* !_IICHID_H_ */
