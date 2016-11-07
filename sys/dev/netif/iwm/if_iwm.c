abc59728531f744e667aef2d83073ec7afa89cd3
Imre Vadász <imre@vdsz.com>
2016-11-07 22:02:31 +0100

    if_iwm - Advertise background scanning support.

--- sys/dev/netif/iwm/if_iwm.c
+++ sys/dev/netif/iwm/if_iwm.c
@@ -6018,8 +6018,8 @@ iwm_attach(device_t dev)
 	    IEEE80211_C_WME |
 	    IEEE80211_C_PMGT |
 	    IEEE80211_C_SHSLOT |	/* short slot time supported */
-	    IEEE80211_C_SHPREAMBLE	/* short preamble supported */
-//	    IEEE80211_C_BGSCAN		/* capable of bg scanning */
+	    IEEE80211_C_SHPREAMBLE |	/* short preamble supported */
+	    IEEE80211_C_BGSCAN		/* capable of bg scanning */
 	    ;
 	/* Advertise full-offload scanning */
 	ic->ic_flags_ext = IEEE80211_FEXT_SCAN_OFFLOAD;
