11c6d3acba432cb452e51f0f1621a9b7ecc1bd64
Imre Vadász <imre@vdsz.com>
2018-04-01 23:08:55 +0200

    hid - Add interval_ms instance variable.

--- sys/bus/hid/hidvar.h
+++ sys/bus/hid/hidvar.h
@@ -13,6 +13,7 @@ enum hid_device_ivars {
 	HID_IVAR_PRODUCT,
 	HID_IVAR_VENDOR,
 	HID_IVAR_SERIAL,
+	HID_IVAR_INTERVAL_MS,
 };
 
 enum hid_bus_type {
@@ -24,5 +25,6 @@ __BUS_ACCESSOR(hid, bustype, HID, BUSTYPE, int)
 __BUS_ACCESSOR(hid, product, HID, PRODUCT, uint16_t)
 __BUS_ACCESSOR(hid, vendor, HID, VENDOR, uint16_t)
 __BUS_ACCESSOR(hid, serial, HID, SERIAL, const char *)
+__BUS_ACCESSOR(hid, interval_ms, HID, INTERVAL_MS, uint16_t)
 
 #endif /* BUS_HID_HIDVAR_H */
