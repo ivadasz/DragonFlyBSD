3029edb6951273dc6a453c58e0b64ec9e146cc73
Imre Vadász <imre@vdsz.com>
2018-04-02 14:33:41 +0200

    hid - Add HID protocol ivar value.

--- sys/bus/hid/hidvar.h
+++ sys/bus/hid/hidvar.h
@@ -7,6 +7,12 @@ enum hid_bootproto {
 	HID_BOOTPROTO_MOUSE,
 };
 
+enum hid_protocol {
+	HID_PROTOCOL_OTHER,
+	HID_PROTOCOL_BOOT_KEYBOARD,
+	HID_PROTOCOL_MOUSE,
+};
+
 enum hid_device_ivars {
 	HID_IVAR_BOOTPROTO,
 	HID_IVAR_BUSTYPE,
@@ -14,6 +20,7 @@ enum hid_device_ivars {
 	HID_IVAR_VENDOR,
 	HID_IVAR_SERIAL,
 	HID_IVAR_INTERVAL_MS,
+	HID_IVAR_PROTOCOL,
 };
 
 enum hid_bus_type {
@@ -26,5 +33,6 @@ __BUS_ACCESSOR(hid, product, HID, PRODUCT, uint16_t)
 __BUS_ACCESSOR(hid, vendor, HID, VENDOR, uint16_t)
 __BUS_ACCESSOR(hid, serial, HID, SERIAL, const char *)
 __BUS_ACCESSOR(hid, interval_ms, HID, INTERVAL_MS, uint16_t)
+__BUS_ACCESSOR(hid, protocol, HID, PROTOCOL, int)
 
 #endif /* BUS_HID_HIDVAR_H */
