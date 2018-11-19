0ab6d9f7d2dc64bda97066b17f89fd83e4a2f85d
Imre Vadász <imre@vdsz.com>
2018-11-19 22:14:05 +0100

    bazel - Add a fixed ioconf.c, to make things build for now.

--- sys/kern/ioconf.c
+++ sys/kern/ioconf.c
@@ -1,5 +1,15 @@
 #include <sys/types.h>
+#include <bus/isa/isareg.h>
 #include <sys/bus_private.h>
 
-struct config_device config_devtab[] = {};
-int devtab_count = 0;
+struct config_resource sio0_resources[] = {
+	{ "at",	RES_STRING,	{ (long)"isa" }},
+	{ "flags",	RES_INT,	{ 0x10 }},
+	{ "port",	RES_INT,	 { IO_COM1 }},
+	{ "irq",	RES_INT,	{ 4 }},
+};
+#define sio0_count 4
+struct config_device config_devtab[] = {
+	{ "sio",	0,	sio0_count,	sio0_resources },
+};
+int devtab_count = 1;
