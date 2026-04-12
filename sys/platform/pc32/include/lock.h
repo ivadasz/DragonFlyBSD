c76eb2114a5c55744269bbb52a4bc8f588ba1e25
Imre Vadász <imre@vdsz.com>
2026-04-13 01:27:10 +0200

    Apply spinlock_deprecated change to i386.

--- sys/platform/pc32/include/lock.h
+++ sys/platform/pc32/include/lock.h
@@ -109,8 +109,6 @@ struct spinlock_deprecated {
 	volatile int	opaque;
 };
 
-typedef struct spinlock_deprecated *spinlock_t;
-
 void	mpintr_lock(void);	/* disables int / spinlock combo */
 void	mpintr_unlock(void);
 void	com_lock(void);		/* disables int / spinlock combo */
@@ -120,15 +118,15 @@ void	imen_unlock(void);
 void	clock_lock(void);	/* disables int / spinlock combo */
 void	clock_unlock(void);
 
-void	spin_lock_deprecated(spinlock_t lock);
-void	spin_unlock_deprecated(spinlock_t lock);
+void	spin_lock_deprecated(struct spinlock_deprecated *lock);
+void	spin_unlock_deprecated(struct spinlock_deprecated *lock);
 
 /*
  * Inline version of spinlock routines -- overrides assembly.  Only unlock
  * and init here please.
  */
 static __inline void
-spin_lock_init(spinlock_t lock)
+spin_init_deprecated(struct spinlock_deprecated *lock)
 {
 	lock->opaque = 0;
 }
