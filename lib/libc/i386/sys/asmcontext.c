7594951f2af9f0eed59e9cfddae1e353af911b43
Imre Vadász <imre@vdsz.com>
2026-04-14 01:23:46 +0200

    Add _quick context functions for i386 to libc.

--- lib/libc/i386/sys/asmcontext.c
+++ lib/libc/i386/sys/asmcontext.c
@@ -4,11 +4,20 @@
 #include <sys/types.h>
 #include <sys/ucontext.h>
 #include <sys/assym.h>
+#include <machine/frame.h>
+#include <machine/tss.h>
+#include <machine/segments.h>
 #include <stddef.h>
 
 ASSYM(UC_SIGMASK, offsetof(ucontext_t, uc_sigmask));
+ASSYM(UC_LINK, offsetof(ucontext_t, uc_link));
 ASSYM(UC_MCONTEXT, offsetof(ucontext_t, uc_mcontext));
 ASSYM(SIG_BLOCK, SIG_BLOCK);
 ASSYM(SIG_SETMASK, SIG_SETMASK);
+ASSYM(_MC_FPOWNED_NONE, _MC_FPOWNED_NONE);
+ASSYM(_MC_FPFMT_NODEV, _MC_FPFMT_NODEV);
+ASSYM(_MC_FPFMT_387, _MC_FPFMT_387);
+ASSYM(_MC_FPFMT_XMM, _MC_FPFMT_XMM);
 ASSYM(MC_LEN, offsetof(mcontext_t, mc_len));
 ASSYM(SIZEOF_MCONTEXT_T, sizeof(mcontext_t));
+ASSYM(SIZEOF_UCONTEXT_T, sizeof(ucontext_t));
